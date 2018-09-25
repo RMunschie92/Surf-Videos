1a4ebb060cf9d02956178c95404186b0b10e777b
Ryan Munsch <rmunsch9632@gmail.com>
2018-09-25 13:58:34 -0400

    search functionality added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,27 +9,40 @@ class Home extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      query: "surf team",
+      query: '',
       items: []
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   API_KEY = "AIzaSyD1cHSIGEpQiTyYr-cuYiWu4cbV7YXIz24";
 
-  componentDidMount() {
-    fetch(`https://www.googleapis.com/youtube/v3/search?key=${this.API_KEY}&part=snippet,id&type=video&q=${this.state.query}&maxResults=50`)
+  fetchData() {
+    let q;
+    this.state.query === "" ? (q = "Surf") : (q = "Surf ".concat(this.state.query));
+    console.log("q: ", q);
+    fetch(`https://www.googleapis.com/youtube/v3/search?key=${this.API_KEY}&part=snippet,id&type=video&q=${q}&maxResults=50`)
       .then(res => res.json())
       .then(result => {
-        this.setState({
-          isLoaded: true,
-          items: result.items
+          this.setState({ isLoaded: true, items: result.items });
+        }, error => {
+          this.setState({ isLoaded: true, error });
         });
-      }, error => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  handleChange(e) {
+    this.setState({ query: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.fetchData();
   }
 
   render() {
@@ -48,12 +61,17 @@ class Home extends Component {
       </li>
     ));
 
-    return(
+    return (
       <div className="Home">
         <header className="header">
           <h1 className="title">Welcome to Surf Videos!</h1>
           <h3 className="slogan">Hang ten, hombre</h3>
         </header>
+        <form onSubmit={this.handleSubmit}>
+          <label>Surf's up, Search up!</label>
+          <input type="text" placeholder="Surf ..." value={this.state.query} onChange={this.handleChange}/>
+          <button>Search</button>
+        </form>
         <ul className="videoList">{this.videoList}</ul>
       </div>
     ) 
