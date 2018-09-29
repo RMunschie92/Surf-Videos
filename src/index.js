410a5e707098159763f9fddb8caac0bfc5e8adb1
Ryan Munsch <rmunsch9632@gmail.com>
2018-09-29 01:03:05 -0400

    react-router-dom import fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/index.css';
 import SurfVideos from './containers/SurfVideos';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <SurfVideos />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
 registerServiceWorker();
